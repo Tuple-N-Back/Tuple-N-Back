7684b6f72b28192f1b90236d6faec424c1373a0b
Stanislav Ivanov <slavivanov2004@gmail.com>
2025-09-06 08:44:18 +0300

    Fix linting

--- app/src/main/java/org/squidfish/tuplenback/presentation/navigation/ScreenDestination.kt
+++ app/src/main/java/org/squidfish/tuplenback/presentation/navigation/ScreenDestination.kt
@@ -4,8 +4,9 @@ import kotlinx.serialization.Serializable
 import org.squidfish.tuplenback.games.Game
 
 sealed interface ScreenDestination {
+    // FIXME: game is temporarily nullable until passing data between screens is reworked
     @Serializable
-    data class GameScreen(val game: Game?): ScreenDestination // game is temporarily nullable until passing data between screens is reworked
+    data class GameScreen(val game: Game?): ScreenDestination
 
     @Serializable
     data class SummaryScreen(val game: Game): ScreenDestination
